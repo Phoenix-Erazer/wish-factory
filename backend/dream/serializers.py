f5f4a427616a428224be825520c974f0591d2366
Andii Rashevskyi <phoenixerazer@gmail.com>
2024-01-02 19:08:11 +0200

    Add BenefactorSerializer in serializers.py

---
+++ backend/dream/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+from backend.dream.models import Benefactor
+
+
+class BenefactorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Benefactor
+        fields = (
+            "id", "full_name", "phone_number", "email",
+            "method_of_receipt", "date_execution", "dream",
+        )
