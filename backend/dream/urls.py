7e85f7abc1ce7e7b900a14389c0b7d368252548d
ZaikaDaria <corplinebusiness@gmail.com>
2024-01-02 22:51:17 +0200

    Add url for DreamViewSet

--- backend/dream/urls.py
+++ backend/dream/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 
 from rest_framework import routers
-
-from .views import BenefactorViewSet
+from .views import (
+    DreamViewSet,
+    BenefactorViewSet,
+)
 
 router = routers.DefaultRouter()
+router.register(r"dreams", DreamViewSet, basename="dream")
 router.register("benefactors", BenefactorViewSet)
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [path("", include(router.urls)), ]
 
 app_name = "dream"
