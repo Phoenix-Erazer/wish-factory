e42b26f95adfdf7439d1cf64a70c81e87fe2beaa
ZaikaDaria <corplinebusiness@gmail.com>
2024-01-05 19:43:43 +0200

    Update Dream model (price)

--- backend/dream/admin.py
+++ backend/dream/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import Dream, Benefactor, Location, Price
+from .models import Dream, Benefactor, Location
 
-admin.site.register(Price)
 admin.site.register(Location)
 admin.site.register(Dream)
 admin.site.register(Benefactor)
