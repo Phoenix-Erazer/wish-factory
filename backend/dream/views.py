4a6479783b120e2dd2a8d0df053b019c364f322a
Andii Rashevskyi <phoenixerazer@gmail.com>
2024-01-07 00:07:20 +0200

    Fixing code using Flake8

--- backend/dream/views.py
+++ backend/dream/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from .models import Dream, Benefactor,
+from .models import Dream, Benefactor
 from .serializers import (
     DreamSerializer,
     BenefactorSerializer,
