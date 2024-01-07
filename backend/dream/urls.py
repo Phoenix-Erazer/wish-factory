d8bc84594e8e8c97a184c4668ba89cb21cc3c557
ZaikaDaria <corplinebusiness@gmail.com>
2024-01-07 23:36:15 +0200

    Add view and serilizer for Location model

--- backend/dream/urls.py
+++ backend/dream/urls.py
@@ -4,11 +4,13 @@ from rest_framework import routers
 from .views import (
     DreamViewSet,
     BenefactorViewSet,
+    LocationViewSet,
 )
 
 router = routers.DefaultRouter()
 router.register(r"dreams", DreamViewSet, basename="dream")
 router.register("benefactors", BenefactorViewSet)
+router.register("location", LocationViewSet)
 
 urlpatterns = [path("", include(router.urls)), ]
 
