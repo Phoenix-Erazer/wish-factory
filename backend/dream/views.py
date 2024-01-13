d6f2ded043b95cae702ccabe71754bae925cfb61
ZaikaDaria <corplinebusiness@gmail.com>
2024-01-13 22:44:52 +0200

    Add execution logic and change Payment to Executor

--- backend/dream/views.py
+++ backend/dream/views.py
@@ -2,12 +2,12 @@ from rest_framework import viewsets, status, mixins
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from .models import Dream, Benefactor, Location
+from .models import Dream, Benefactor, Location, Execution
 from .serializers import (
     DreamSerializer,
     BenefactorSerializer,
     LocationSerializer,
-    PaymentSerializer
+    ExecutionSerializer
 )
 
 
@@ -21,21 +21,20 @@ class DreamViewSet(mixins.ListModelMixin,
     serializer_class = DreamSerializer
 
     @action(detail=True, methods=["post"])
-    def dream_payment(self, request, pk=None):
+    def dream_execution(self, request, pk=None):
         dream = self.get_object()
 
         payment_data = {
-            "amount": dream.price,
-            "currency": dream.currency,
+            "dream": dream.id,
+            "success": True,
         }
 
-        payment_data["success"] = True
-
-        payment_serializer = PaymentSerializer(data=payment_data)
+        payment_serializer = ExecutionSerializer(data=payment_data)
         payment_serializer.is_valid(raise_exception=True)
         payment_instance = payment_serializer.save()
 
         if payment_instance.success:
+            dream.is_activated = False
             dream.save()
             return Response({"message": "Dream paid successfully"})
         else:
@@ -67,19 +66,18 @@ class BenefactorViewSet(mixins.ListModelMixin,
                         mixins.UpdateModelMixin,
                         mixins.DestroyModelMixin,
                         viewsets.GenericViewSet):
-
     queryset = Benefactor.objects.all()
     serializer_class = BenefactorSerializer
 
-    def perform_create(self, serializer):
-        benefactor_instance = serializer.save()
-
-        related_dream = benefactor_instance.dream
-
-        related_dream.is_activated = False
-        related_dream.save()
-
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    # def perform_create(self, serializer):
+    #     benefactor_instance = serializer.save()
+    #
+    #     related_dream = benefactor_instance.dream
+    #
+    #     related_dream.is_activated = False
+    #     related_dream.save()
+    #
+    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class LocationViewSet(mixins.ListModelMixin,
@@ -90,3 +88,13 @@ class LocationViewSet(mixins.ListModelMixin,
                       viewsets.GenericViewSet):
     queryset = Location.objects.all()
     serializer_class = LocationSerializer
+
+
+class ExecutionViewSet(mixins.ListModelMixin,
+                       mixins.CreateModelMixin,
+                       mixins.RetrieveModelMixin,
+                       mixins.UpdateModelMixin,
+                       mixins.DestroyModelMixin,
+                       viewsets.GenericViewSet):
+    queryset = Execution.objects.all()
+    serializer_class = ExecutionSerializer
