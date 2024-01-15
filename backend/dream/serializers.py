cac4759decec8f5056d81ed0732aa25062f69992
ZaikaDaria <corplinebusiness@gmail.com>
2024-01-15 18:32:35 +0200

    Reveal the fields in serializers.py

--- backend/dream/serializers.py
+++ backend/dream/serializers.py
@@ -6,16 +6,27 @@ from .models import Dream, Benefactor, Payment
 class DreamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dream
-        fields = "__all__"
+        fields = (
+            "title", "description", "dream_type", "user_name",
+            "user_age", "user_email", "user_type", "date", "price",
+            "currency", "attachment", "city", "region", "country",
+            "is_activated",
+        )
 
 
 class BenefactorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Benefactor
-        fields = "__all__"
+        fields = (
+            "full_name", "phone_number", "email",
+            "method_of_receipt", "date_execution",
+            "dream",
+        )
 
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
-        fields = "__all__"
+        fields = (
+            "amount", "currency", "success", "timestamp",
+        )
