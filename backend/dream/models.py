76a43b32946bd973032d7d0932cc0ac167096695
Andii Rashevskyi <phoenixerazer@gmail.com>
2024-01-16 00:59:25 +0200

    fix media in Dream.attachment

--- backend/dream/models.py
+++ backend/dream/models.py
@@ -1,7 +1,11 @@
+import os
+import uuid
+
 from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 from phone_field import PhoneField
 
 USER_TYPE_CHOICES = [
@@ -27,6 +31,14 @@ def not_past_date_validator(value):
         raise ValidationError('Date cannot be in the past')
 
 
+def dream_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads", "dreams", filename)
+
+
 class Dream(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(max_length=500)
@@ -38,8 +50,9 @@ class Dream(models.Model):
     date = models.DateField()
     price = models.FloatField(validators=[MinValueValidator(0)])
     currency = models.CharField(max_length=3, default="USD")
-    attachment = models.FileField(
-        upload_to="attachments/", null=True, blank=True)
+    attachment = models.ImageField(
+        upload_to=dream_image_file_path, null=True, blank=True
+    )
 
     city = models.CharField(max_length=255)
     region = models.CharField(max_length=255)
