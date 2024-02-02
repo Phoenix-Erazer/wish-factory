5bb179701de1fb20e03a143e09637ffc6c861349
Andii Rashevskyi <phoenixerazer@gmail.com>
2024-02-02 20:43:09 +0200

    fix code

--- wish_factory/settings.py
+++ wish_factory/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 # ALLOWED_HOSTS = ["127.0.0.1", "wish-factory.onrender.com", "wish-factory-docker.onrender.com", "victorgri.github.io/wish_demo",]
-ALLOWED_HOSTS = ["*", ]
+ALLOWED_HOSTS = ["*" ]
 
 # Application definition
 
